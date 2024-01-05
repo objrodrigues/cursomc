83801f07bcb16e3c256a2b1ec158f6a605962637
Diego Rodrigues <objrodrigues@gmail.com>
2024-01-05 14:30:10 -0300

    Criando Repository e Service para Categoria

--- src/main/java/br/com/objrodrigues/cursomc/resource/CategoriaResource.java
+++ src/main/java/br/com/objrodrigues/cursomc/resource/CategoriaResource.java
@@ -1,29 +1,27 @@
 package br.com.objrodrigues.cursomc.resource;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.objrodrigues.cursomc.domain.Categoria;
+import br.com.objrodrigues.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<?> buscar(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria obj = service.buscar(id);
 		
-		List<Categoria> lista = new ArrayList<>();
-		
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		return ResponseEntity.ok().body(obj);
 	}
 }
